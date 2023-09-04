cf046131793a3ab266b73ae8e8d616135ae5138b
welcomepk <kamblepramod806@gmail.com>
2023-09-04 17:04:21 +0530

    layout added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Vans from './pages/Vans'
 import VanDetail from './pages/VanDetail'
-
+import Layout from './components/Layout'
 
 import './server'
 import './App.css'
@@ -15,18 +15,13 @@ import './App.css'
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">LOGO</Link>
-        <nav>
-          <Link to='/about'>About</Link>
-          <Link to='/vans'>Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans />} />
+          <Route path='/vans/:id' element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+function Header() {
+    return (
+        <header>
+            <Link to="/">LOGO</Link>
+            <nav>
+                <Link to='/about'>About</Link>
+                <Link to='/vans'>Vans</Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
